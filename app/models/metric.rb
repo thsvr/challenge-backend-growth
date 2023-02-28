3db142e51346926bded1e4282c6f3c5ed970ee2d
thais <vr.ths.zd@gmail.com>
2023-02-28 15:56:24 -0300

    Sort metrics by timestamp

--- app/models/metric.rb
+++ app/models/metric.rb
@@ -2,4 +2,6 @@ class Metric < ApplicationRecord
     validates :value, presence: true
     validates :name,  presence: true
     validates :timestamp, presence: true
+
+    default_scope { order("timestamp ASC") }
 end
