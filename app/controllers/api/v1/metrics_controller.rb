72683f7567764d948698a8215f5af0017b78cc74
thais <vr.ths.zd@gmail.com>
2023-02-28 15:59:13 -0300

    Create logic to add new metric

--- app/controllers/api/v1/metrics_controller.rb
+++ app/controllers/api/v1/metrics_controller.rb
@@ -5,5 +5,27 @@ class Api::V1::MetricsController < ApplicationController
   end
 
   def create
+    metric = Metric.new(
+      timestamp: metric_params[:timestamp],
+      name: metric_params[:name],
+      value: metric_params[:value]
+    )
+
+    if metric.save
+      render json: metric, status: 200
+    else
+      render json: {
+        error: "Something went wrong..."
+      }
+    end
+  end
+
+  private
+  def metric_params
+    params.require(:metric).permit([
+      :timestamp,
+      :name,
+      :value
+  ])
   end
 end
