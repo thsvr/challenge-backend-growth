b3cc7ad721c45ea7b87cac627ddc11f2d291068f
thais <vr.ths.zd@gmail.com>
2023-02-28 16:19:24 -0300

    Create fake data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Metric.destroy_all
+
+10.times do |index|
+  Metric.create!(name: 'Views',
+                timestamp: Faker::Time.between(from: 5.days.ago, to: 3.hours.ago),
+                value: Faker::Number.number(digits: 1))
+end
+
+15.times do |index|
+  Metric.create!(name: 'Clicks',
+                timestamp: Faker::Time.between(from: 5.days.ago, to: 3.hours.ago),
+                value: Faker::Number.number(digits: 1))
+end
