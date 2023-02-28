c2f3b39cb793f0f39eac88745125203b22cd5ba0
thais <vr.ths.zd@gmail.com>
2023-02-28 15:52:36 -0300

    Add resources to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get 'metrics/index'
-      get 'metrics/create'
+      resources :metrics, only: [:index, :create]
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
